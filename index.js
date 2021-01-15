4d7328c9963eb83c38d82b4048a9251425bd77e7
MatsievichSasha <matsievich.sasha@gmail.com>
2021-01-15 15:41:31 +0200

    Handle errors using try...catch statement

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ const API_URL = 'https://api.github.com/';
 const rootElement = document.getElementById('root');
 const loadingElement = document.getElementById('loading-overlay');
 
-const startApp = function () {
-  const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
-  const fightersPromise = callApi(endpoint, 'GET');
+async function startApp() {
+  try {
+    loadingElement.style.visibility = 'visible';
 
-  fightersPromise.then(fighters => {
-    const fightersNames = getFightersNames(fighters);
-    rootElement.innerText = fightersNames;
-  });
+    const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
+    const fighters = await callApi(endpoint, 'GET');
+
+    rootElement.innerText = getFightersNames(fighters);
+  } catch (error) {
+    console.warn(error);
+    rootElement.innerText = 'Failed to load data';
+  } finally {
+    loadingElement.style.visibility = 'hidden';
+  }
 }
 
 function callApi(endpoind, method) {
-  const url = API_URL + endpoind;
+  const url = API_URL + endpoind
   const options = {
     method
   };
 
   return fetch(url, options)
-    .then(response => response.ok ? response.json() : Promise.reject(Error('Failed to load')))
+    .then(response =>
+      response.ok
+        ? response.json()
+        : Promise.reject(Error('Failed to load'))
+    )
     .then(file => JSON.parse(atob(file.content)))
-    .catch(error => {
-      console.warn(error);
-      rootElement.innerText = 'Failed to load data';
-    })
-    .finally(() => {
-      loadingElement.remove();
-    });
+    .catch(error => { throw error });
 }
 
 function getFightersNames(fighters) {
