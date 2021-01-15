58cf4c9ac3e96aa64481a085ce99983924b0072c
MatsievichSasha <matsievich.sasha@gmail.com>
2021-01-15 14:51:46 +0200

    Add more Functions

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
-const responsePromise = fetch(API_URL);
-/* console.log(responsePromise); */
-
-/* responsePromise
-  .then(response => { console.log(response) }) */
-
-/* responsePromise
-  .then(response => { response.json() }) */ //json також ассинхр отримаємо такж проміс
-
+const API_URL = 'https://api.github.com/';
 const rootElement = document.getElementById('root');
 const loadingElement = document.getElementById('loading-overlay');
-/* rootElement.innerText = 'Loading...'; */
 
-fetch(API_URL)
-  .then(
-    /* response => { return response.json(); }, */
-    /* error => { console.warn(error); } )*/ //друга фнукція обробка помилок
-    response => {
-      if (!response.ok) {
-        throw new Error('Failed load data'); //якщо запрос виповнився але такого обєкту не існує
-      }
-      return response.json();
-    })
-  .then(file => {
-    /* console.log(file) */
-    const fighters = JSON.parse(atob(file.content));//розкодування "base64" глобаний метод atob поверне результат як строку
-    /* console.log(fighters); */
-    const names = fighters.map(it => it.name).join('\n');
-    console.log(names);
-    rootElement.innerText = names;
-    /* loadingElement.remove(); */
-  })
-  .catch(error => {
-    console.warn(error);
-    root.innerText = 'Failed to load data';
-    /* loadingElement.remove(); */
-  })
-  .finally(() => {
-    loadingElement.remove();  //незалежно від того буде помилка чи ні  loadingElement.remove() виповниться
+const startApp = function () {
+  const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
+  const fightersPromise = callApi(endpoint, 'GET');
+
+  fightersPromise.then(fighters => {
+    const fightersNames = getFightersNames(fighters);
+    rootElement.innerText = fightersNames;
   });
+}
 
+function callApi(endpoind, method) {
+  const url = API_URL + endpoind;
+  const options = {
+    method
+  };
+
+  return fetch(url, options)
+    .then(response => response.ok ? response.json() : Promise.reject(Error('Failed to load')))
+    .then(file => JSON.parse(atob(file.content)))
+    .catch(error => {
+      console.warn(error);
+      rootElement.innerText = 'Failed to load data';
+    })
+    .finally(() => {
+      loadingElement.remove();
+    });
+}
+
+function getFightersNames(fighters) {
+  const names = fighters.map(it => it.name).join('\n');
+  return names;
+}
+
+startApp();
