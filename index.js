094e64f00978d96403cf7847245a758a5e6ac7fc
MatsievichSasha <matsievich.sasha@gmail.com>
2021-01-15 14:18:08 +0200

    promise error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
+const API_URL = 'https://api.git--hub.com/repos/sahanr/street-fighter/contents/fighters.json';
 const responsePromise = fetch(API_URL);
 /* console.log(responsePromise); */
 
@@ -8,15 +8,19 @@ const responsePromise = fetch(API_URL);
 /* responsePromise
   .then(response => { response.json() }) */ //json також ассинхр отримаємо такж проміс
 
+const rootElement = document.getElementById('root');
+rootElement.innerText = 'Loading...';
 
 fetch(API_URL)
-  .then(response => response.json())
+  .then(
+    response => { return response.json(); },
+    error => { console.warn(error); }) //друга фнукція обробка помилок
   .then(file => {
     /* console.log(file) */
     const fighters = JSON.parse(atob(file.content));//розкодування "base64" глобаний метод atob поверне результат як строку
     /* console.log(fighters); */
-
     const names = fighters.map(it => it.name).join('\n');
     console.log(names);
+    rootElement.innerText = names;
   });
 
